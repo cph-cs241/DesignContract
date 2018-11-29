91cb2bfd8e855de88e1b74bc3f789b78050126bd
mahnazzzz <mahnaazi@yahoo.com>
2018-11-29 01:14:47 +0100

    changes

--- DesignByContract/Program.cs
+++ DesignByContract/Program.cs
@@ -15,21 +15,21 @@ namespace DesignByContract
         static void Main()
         {
 
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new form());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new form());
 
-            //Account a1 = new Account("A1", 100.1);
-            //Account a2 = new Account("A2", 200.1);
-            //a1.Deposit(50);
-            //a1.Withdraw(10.9);
-            //a1.Withdraw(7);
-            //a2.Deposit(20);
-            //a2.Withdraw(150.9);
-            //System.Diagnostics.Debug.Write("Name is : " + a1.name);
-            //System.Diagnostics.Debug.Write("\n Account is :" + a1.amount + "\n");
-            //System.Diagnostics.Debug.Write("Name is : " + a2.name);
-            //System.Diagnostics.Debug.Write("\n Account is :" + a2.amount + "\n");
+            Account a1 = new Account("A1", 100.1);
+            Account a2 = new Account("A2", 200.1);
+            a1.Deposit(50);
+            a1.Withdraw(10.9);
+            a1.Withdraw(7);
+            a2.Deposit(20);
+            a2.Withdraw(150.9);
+            System.Diagnostics.Debug.Write("Name is : " + a1.name);
+            System.Diagnostics.Debug.Write("\n Account is :" + a1.amount + "\n");
+            System.Diagnostics.Debug.Write("Name is : " + a2.name);
+            System.Diagnostics.Debug.Write("\n Account is :" + a2.amount + "\n");
         }
     }
 }
